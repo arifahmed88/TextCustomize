c6100909c0dc12da0f01f8e73b61a414b4fa3778
Sagar Chandra Das <sagar@bitmorpher.com>
2022-12-21 13:29:43 +0600

    code clean has benn done

--- TextStrock/CustomView/CustoLabel/CurveUILabel.swift
+++ TextStrock/CustomView/CustoLabel/CurveUILabel.swift
@@ -9,47 +9,20 @@ import UIKit
 
 @IBDesignable
 class CurveUILabel: UILabel {
+//    var inflection:CGFloat = 0.0
     
     @IBInspectable var angle: CGFloat = 1.5708
     @IBInspectable var clockwise: Bool = true
-    
     var sliderValue:CGFloat = 0.0
     
-    var inflection:CGFloat = 0.0
-    
     override func drawText(in rect: CGRect) {
-
-        let text = self.text ?? "arif"
-//        drawTextImageTemp(text: text, inflection: self.inflection, size: rect.size)
         centreArcPerpendicular()
-
     }
     
-    
-//    override func draw(_ rect: CGRect) {
-////        centreArcPerpendicular()
-//
-////        let text = self.text ?? "arif"
-////        let nsText = text as NSString
-////        let attributes = [
-////            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24),
-////            NSAttributedString.Key.foregroundColor: UIColor.red
-////        ]
-////        nsText.draw(at: CGPoint(x: 0, y: 0), withAttributes: attributes)
-//
-////        let size =
-//        drawTextImageTemp(text: text, inflection: 100.0, size: rect.size)
-//    }
-//
-    /**
-     This draws the self.text around an arc of radius r,
-     with the text centred at polar angle theta
-     */
     func centreArcPerpendicular() {
         guard let context = UIGraphicsGetCurrentContext() else { return }
         let str = self.text ?? ""
         let size = self.bounds.size
-        ///size.width = size.width
         context.translateBy(x: size.width / 2, y: (size.height / 2)+sliderValue)
         
         let radius = getRadiusForLabel()
@@ -91,9 +64,6 @@ class CurveUILabel: UILabel {
     }
     
     func chordToArc(_ chord: CGFloat, radius: CGFloat) -> CGFloat {
-        // *******************************************************
-        // Simple geometry
-        // *******************************************************
         return 2 * asin(chord / (2 * radius))
     }
     
@@ -135,20 +105,35 @@ class CurveUILabel: UILabel {
         print("value = \(value)")
         return value
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    ///
-    ///
-    ///
-    ///
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// another approach
+extension CurveUILabel{
+    
     // `c1` is the first side of the triangle
     // `c2` is the second side
     // The function returns the hypotenuse
@@ -165,119 +150,6 @@ class CurveUILabel: UILabel {
       // y = sqrt(r^2 - x^2)
       return sqrt(pow(r, 2)-pow(x, 2))
     }
-    
-//    func drawTextImage(text: String, inflection: CGFloat, size: CGSize) -> UIImage {
-//      // Create a format for the renderer to set the scale to 1
-//      let format = UIGraphicsImageRendererFormat()
-//      format.scale = 1
-//
-//      // Create the renderer
-//      let renderer = UIGraphicsImageRenderer(size: size, format: format)
-//
-//      // Ask the renderer to create an image
-//      let image = renderer.image { rendererContext in
-//        let context = rendererContext.cgContext
-//
-//        // Split the string in characters and convert them to NSString so
-//        // we can leverage .size and .draw method
-//        let chars = text.map { String($0) as NSString }
-//        let nsText = text as NSString
-//
-//          let attributes = [
-//          NSAttributedString.Key.font: UIFont.systemFont(ofSize: 72),
-//          NSAttributedString.Key.foregroundColor: UIColor.red
-//        ]
-//
-//        // Compute the size of the text. We need the width to compute the radius of the circle
-//        let textSize = nsText.size(withAttributes: attributes)
-//
-//        // Get the center of the space where we need to draw the text
-//        let center = CGPoint(
-//          x: (size.width - textSize.width) / 2,
-//          y: (size.height - textSize.height) / 2
-//        )
-//
-//        // Handle the edge case where we don't want a curved text. Just draw the nsText
-//        guard inflection != 0 else {
-//          nsText.draw(at: center, withAttributes: attributes)
-//          return
-//        }
-//
-//        // Create a variable that works as accumulator to draw the single letters
-//        var startPoint = center
-//
-//        // Compute the second side of the triangle. It depends on how curved we want the text.
-//        let c2 = abs(inflection * textSize.width / 2)
-//
-//        // Compute the radius of the circumference
-//        let r = radius(c1: textSize.width / 2, c2: c2)
-//
-//        // Start drawinf the single characters
-//        for c in chars {
-//          // Save the current state of the context
-//          context.saveGState()
-//
-//          // Compute the size of the letter to draw
-//          let cSize = c.size(withAttributes: attributes)
-//
-//          // Compute the leftmost x-coordinate of the letter.
-//          // we need to subtract half of the text width to make sure that the center
-//          // is in the same x-coordinate of the center of the text we want to draw
-//          let x = (startPoint.x - center.x) - textSize.width/2
-//          // Compute the y-coordinate. It is multiplied by the inflection to ensure a smooth behavior
-//          let y = inflection * halfCircle(x: x, r: r)
-//
-//          // Compute the mid point of the letter, we need this to position the context
-//          // correctly before drawing the letter
-//          let xm = (startPoint.x + cSize.width/2 - center.x) - textSize.width / 2
-//          let ym = inflection * halfCircle(x: xm, r: r)
-//
-//          // Compute the rightmost point of the letter
-//          let x2 = ((startPoint.x + cSize.width) - center.x) - textSize.width / 2
-//          let y2 = inflection * halfCircle(x: x2, r: r)
-//
-//          // Compute the slope and the rotation
-//          let m = (y2 - y) / (x2 - x)
-//          let theta: CGFloat = atan(m)
-//
-//          // Compute the y position for the letter.
-//          // from the center of the screen, we need to move by the `ym` value of the letter
-//          // and we need to subtract how much we moved the center of the circumference.
-//          // If we do not add the `- inflection * c2`, we are basically keeping the
-//          // center of the circle in the center of our paper. Therefore, the text will move
-//          // up or down. Instead, we want to keep it in the middle of the view.
-//          startPoint.y = center.y + ym - inflection * c2
-//
-//          // First, lets move the context so that it is centered in the letter.
-//          context.translateBy(
-//            x: startPoint.x + cSize.width/2,
-//            y: startPoint.y + cSize.height/2
-//          )
-//
-//          // Rotate the paper around the center of the letter
-//          context.rotate(by: theta)
-//
-//          // Draw the letter. We need to draw the letter at `(-cSize.width/2, -cSize.height/2)` because
-//          // CoreGraphics draws the letter from their top left corner.
-//          c.draw(
-//            at: .init(
-//              x: -cSize.width/2,
-//              y: -cSize.height/2
-//            ),
-//            withAttributes: attributes)
-//
-//          // Update the accumulator so that we move on the x axis to render the next letter.
-//          startPoint.x = startPoint.x + cSize.width
-//
-//          // Restore the context
-//          context.restoreGState()
-//        }
-//      }
-//      return image
-//    }
-    
-    
-    
     
     func drawTextImageTemp(text: String, inflection: CGFloat, size: CGSize) {
         
@@ -385,12 +257,4 @@ class CurveUILabel: UILabel {
         
         
     }
-
-    
-    
-    
-    
-    
-    
-    
 }
--- TextStrock/ViewControllers/TextCurveViewController.swift
+++ TextStrock/ViewControllers/TextCurveViewController.swift
@@ -10,8 +10,7 @@ import SnapKit
 
 
 class TextCurveViewController: UIViewController {
-    @IBOutlet weak var label: CurveUILabel!
-    
+
     let testLabel = CurveUILabel()
     let curveLabel = DGCurvedLabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
     
@@ -33,22 +32,6 @@ class TextCurveViewController: UIViewController {
         testLabel.textAlignment = .center
         view.addSubview(testLabel)
         
-        
-        //
-//        curveLabel.text = "arif ahmed auny"
-//        curveLabel.font = UIFont(name: "Chalkduster", size: 24)
-//        curveLabel.textColor = UIColor.systemPink
-//
-////        let attributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.blue,.font: UIFont(name: "Chalkduster", size: 24) ?? UIFont.boldSystemFont(ofSize: CGFloat(24))]
-////        curveLabel.attributedText = NSMutableAttributedString(string: "arif ahmed auny", attributes: attributes)
-//        curveLabel.textInside = false
-//        curveLabel.radius = 100
-//
-//        curveLabel.backgroundColor = .lightGray
-//        curveLabel.sizeToFit()
-//        view.addSubview(curveLabel)
-//        curveLabel.center = CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
-        
         slider.value = sliderValue
         slider.minimumValue = sliderMinValue
         slider.maximumValue = sliderMaxValue
@@ -66,26 +49,7 @@ class TextCurveViewController: UIViewController {
         testLabel.frame = CGRect(origin: CGPoint(x: originX, y: originY), size: CGSize(width: UIScreen.main.bounds.width - 2*originX, height: 200.0))
         testLabel.backgroundColor = UIColor.hexStringToUIColor(hex: "#EEEEEE")
         testLabel.textColor = .systemBlue
-        testLabel.text = "This of the CurveText. are iu i am"
-//        testLabel.isHidden = true
-        
-        
-        //test
-//        curveLabel.frame = CGRect(origin: CGPoint(x: originX, y: originY), size: CGSize(width: UIScreen.main.bounds.width - 2*originX, height: 600.0))
-//        curveLabel.center = CGPoint(x: UIScreen.main.bounds.width*0.5, y: UIScreen.main.bounds.height*0.5)
-        
-//        testview.backgroundColor = .systemMint
-//
-//        testview.center = CGPoint(x: UIScreen.main.bounds.width*0.5, y: UIScreen.main.bounds.height*0.5)
-//        testview.layer.addSublayer(testCAlayer)
-//
-//        view.addSubview(testview)
-        
-        
-        
-        
-        
-        
+        testLabel.text = "This of the CurveText. I am Who I a'm"
     }
 
     @IBAction func closeButtonAction(_ sender: Any) {
@@ -96,112 +60,8 @@ class TextCurveViewController: UIViewController {
         let value = CGFloat(sender.value)
         sliderLabel.text = "\(Int(value))"
         
-        testLabel.inflection = value
-        
-        
         testLabel.sliderValue = value
         testLabel.setNeedsDisplay()
         
-//        curveLabel.radius = value
-//        curveLabel.setNeedsDisplay()
-        
-        
-//        testCAlayer = CALayer()
-//
-//        drawCurvedString(
-//          on: testCAlayer,
-//          text: NSAttributedString(
-//            string: "This is a test string",
-//            attributes: [
-//                NSAttributedString.Key.foregroundColor: UIColor.purple,
-//                NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15)
-//            ]),
-//          angle: 270,
-//          radius: value)
     }
-    
-
 }
-
-//
-
-//extension TextCurveViewController{
-//    func drawCurvedString(on layer: CALayer, text: NSAttributedString, angle: CGFloat, radius: CGFloat) {
-//      var radAngle = angle.radians
-//
-//      let textSize = text.boundingRect(
-//        with: CGSize(width: .max, height: .max),
-//        options: [.usesLineFragmentOrigin, .usesFontLeading],
-//        context: nil)
-//      .integral
-//      .size
-//
-//      let perimeter: CGFloat = 2 * .pi * radius
-//      let textAngle: CGFloat = textSize.width / perimeter * 2 * .pi
-//
-//      var textRotation: CGFloat = 0
-//      var textDirection: CGFloat = 0
-//
-//      if angle > CGFloat(10).radians, angle < CGFloat(170).radians {
-//        // bottom string
-//        textRotation = 0.5 * .pi
-//        textDirection = -2 * .pi
-//        radAngle += textAngle / 2
-//      } else {
-//        // top string
-//        textRotation = 1.5 * .pi
-//        textDirection = 2 * .pi
-//        radAngle -= textAngle / 2
-//      }
-//
-//      for c in 0..<text.length {
-//        let letter = text.attributedSubstring(from: NSRange(c..<c+1))
-//        let charSize = letter.boundingRect(
-//          with: CGSize(width: .max, height: .max),
-//          options: [.usesLineFragmentOrigin, .usesFontLeading],
-//          context: nil)
-//        .integral
-//        .size
-//
-//        let letterAngle = (charSize.width / perimeter) * textDirection
-//        let x = radius * cos(radAngle + (letterAngle / 2))
-//        let y = radius * sin(radAngle + (letterAngle / 2))
-//
-//        let singleChar = drawText(
-//          on: layer,
-//          text: letter,
-//          frame: CGRect(
-//            x: (layer.frame.size.width / 2) - (charSize.width / 2) + x,
-//            y: (layer.frame.size.height / 2) - (charSize.height / 2) + y,
-//            width: charSize.width,
-//            height: charSize.height))
-//        layer.addSublayer(singleChar)
-//        singleChar.transform = CATransform3DMakeAffineTransform(CGAffineTransform(rotationAngle: radAngle - textRotation))
-//        radAngle += letterAngle
-//      }
-//    }
-//
-//
-//    func drawText(on layer: CALayer, text: NSAttributedString, frame: CGRect) -> CATextLayer {
-//      let textLayer = CATextLayer()
-//      textLayer.frame = frame
-//      textLayer.string = text
-//        textLayer.alignmentMode = CATextLayerAlignmentMode.center
-//      textLayer.contentsScale = UIScreen.main.scale
-//      return textLayer
-//    }
-//
-//
-//}
-
-//extension CGFloat {
-//  /** Degrees to Radian **/
-//  var degrees: CGFloat {
-//    return self * (180.0 / .pi)
-//  }
-//
-//  /** Radians to Degrees **/
-//  var radians: CGFloat {
-//    return self / 180.0 * .pi
-//  }
-//}
